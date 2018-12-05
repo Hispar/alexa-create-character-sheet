43677bc9f243c2ee01dee67652d8bd5cd235b745
pburgos <pburgos@mrmilu.com>
2018-12-05 15:47:17 +0100

    Testing deployment for lambda

--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -207,6 +207,7 @@ class ResponseLogger(AbstractResponseInterceptor):
 
 # Register intent handlers
 sb.add_request_handler(GetNewFactHandler())
+sb.add_request_handler(CreateCharacterIntent())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(FallbackIntentHandler())

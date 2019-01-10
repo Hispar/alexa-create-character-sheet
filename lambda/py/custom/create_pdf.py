55b26116ea1ec196d94defc30f705781c703bd24
pburgos <pburgos@mrmilu.com>
2019-01-10 17:31:43 +0100

    Testing pdf generation new edition

--- lambda/py/custom/create_pdf.py
+++ lambda/py/custom/create_pdf.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Python imports
 import logging
-import os
 
 # 3rd party imports
 import pdfkit
@@ -11,11 +10,7 @@ import pdfkit
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-here = lambda *x: os.path.abspath(os.path.join(BASE_DIR, *x))
-
-binary_path = here('wkhtmltopdf')
-configuration = pdfkit.configuration(wkhtmltopdf=binary_path)
+configuration = pdfkit.configuration(wkhtmltopdf=binaries/wkhtmltopdf)
 
 
 class PdfRenderer(object):
